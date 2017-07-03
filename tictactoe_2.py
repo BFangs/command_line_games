54314a9cfb27077fdfa5aaa4b493f056dcff452c
vagrant <vagrant@vagrant.vm>
2017-07-03 19:17:16 +0000

    added battleship

--- tictactoe_2.py
+++ tictactoe_2.py
@@ -3,10 +3,16 @@
 class Board(object):
     """3x3 board"""
 
-    def __init__(self):
-        self.grid = [["-","-","-"],
-                     ["-","-","-"],
-                     ["-","-","-"]]
+    def __init__(self, grid=[["-","-","-"],
+                             ["-","-","-"],
+                             ["-","-","-"]]):
+        self.grid = grid
+
+    @classmethod
+    def starting_board(cls, grid=[["-","-","-"],
+                                  ["-","-","-"],
+                                  ["-","-","-"]]):
+        board = cls(grid)
 
     def print_board(self):
         """printing the board"""
@@ -14,17 +20,31 @@ class Board(object):
         for row in self.grid:
             print "%s|%s|%s" % (row[0], row[1], row[2])
 
-    def add_token(self, x, y):
+    def get_board(self):
+        """returning board"""
+        return self.grid
+
+    def move(self, x, y, player):
         """adding a token user side"""
+        if player != "X" or player != "O":
+            print "Sorry, that isn't a valid player"
+            return False
         if (0<x<4) and (0<y<4) and isinstance( x, int ) and isinstance( y, int ):
             if self.grid[-y][x-1] == "-":
-                self.grid[-y][x-1] = "X"
+                self.grid[-y][x-1] = player
+                return True
             else:
                 print "Sorry that spot is taken!"
+                return False
         else:
             print "Sorry, that isn't part of the board"
+            return False
 
-    def check_board(self):
+    def is_winner(self, player):
+        """checking if player has won"""
+        pass
+
+    def is_full(self):
         """check if board is full"""
         for row in self.grid:
             for spot in row:
@@ -34,3 +54,4 @@ class Board(object):
 
 class Computer(object):
     """computer to make moves on board"""
+    pass
